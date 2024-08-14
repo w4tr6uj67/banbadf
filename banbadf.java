e75e0a9038ed6c5fd3a219c503c73efc7359d393
w4tr6uj67 <ThakorBhoyar542@gmail.com>
2024-08-14 18:31:04 +0800

    Create banbadf.java

---
+++ banbadf.java
@@ -0,0 +1,47 @@
+/* 文件名 : AbstractDemo.java */
+public class AbstractDemo
+{
+   public static void main(String [] args)
+   {
+      Salary s = new Salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+      Employee e = new Salary("John Adams", "Boston, MA", 2, 2400.00);
+ 
+      System.out.println("Call mailCheck using Salary reference --");
+      s.mailCheck();
+ 
+      System.out.println("\n Call mailCheck using Employee reference--");
+      e.mailCheck();
+    }
+}
+public abstract class Employee
+{
+   private String name;
+   private String address;
+   private int number;
+   
+   public abstract double computePay();
+   
+   //其余代码
+}
+/* 文件名 : Salary.java */
+public class Salary extends Employee
+{
+   private double salary; // Annual salary
+  
+   public double computePay()
+   {
+      System.out.println("Computing salary pay for " + getName());
+      return salary/52;
+   }
+ 
+   //其余代码
+}
+/* 文件名 : NameOfInterface.java */
+import java.lang.*;
+//引入包
+ 
+public interface NameOfInterface
+{
+   //任何类型 final, static 字段
+   //抽象方法
+}
